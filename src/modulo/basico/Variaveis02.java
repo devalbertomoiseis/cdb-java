5c69835b64b2ad7e3c771044971f20d7b755401b
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-09-26 18:31:14 -0300

    CDB - JAVA: Trabalhando If e Else

---
+++ src/modulo/basico/Variaveis02.java
@@ -0,0 +1,16 @@
+package modulo.basico;
+
+import java.util.Scanner;
+
+public class Variaveis02 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite seu sobre nome: ");
+        String sobreNome = scanner.nextLine();
+
+        System.out.println("Nome: " + nome + " Sobre Nome: " + sobreNome);
+    }
+}
