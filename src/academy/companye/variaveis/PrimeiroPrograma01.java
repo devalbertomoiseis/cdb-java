b57ef1b3bbcb0063ec2a8c185da148df56220589
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-10-11 10:35:34 -0300

    CDB - JAVA: Trabalhando com Exercicios

--- src/academy/companye/variaveis/PrimeiroPrograma01.java
+++
@@ -1,8 +0,0 @@
-package academy.companye.variaveis;
-
-public class PrimeiroPrograma01 {
-    public static void main(String[] args) {
-        System.out.println("Hello World.. Olá, Mundo !!");
-        System.out.println("Esse sistema é muito Bom");
-    }
-}
